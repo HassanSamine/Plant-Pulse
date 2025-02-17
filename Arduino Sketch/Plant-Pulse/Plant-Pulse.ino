2bc0d12b26e566d41ef522f55d065e13e1860cda
HassanSamine <saminehassan@gmail.com>
2025-02-17 19:03:58 +0100

    Minor Code Updates

--- Arduino Sketch/Plant-Pulse/Plant-Pulse.ino
+++ Arduino Sketch/Plant-Pulse/Plant-Pulse.ino
@@ -9,38 +9,45 @@ int LED = 6;
 int PIR_State = 0;
 int RELAY= 13;
 int sensorPin = A0; 
+int SensorMoist;
 int Moist;
 int Temp;
 int Humid;
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16,2); //Initialize the 16x2 LCD
-  lcd.setCursor(0,0);
-  lcd.print("Plant Pulse - A Project by Hassan Samine.");
   pinMode(PIR, INPUT); //Set pin as input
   pinMode(LED,OUTPUT); //Set pin as output
   pinMode(RELAY,OUTPUT);
+  lcd.begin(16,2); //Initialize the 16x2 LCD
+  lcd.setCursor(0,0);
+  lcd.print("Plant Pulse - A Project by Hassan Samine.");
+  delay(1000);
+  lcd.clear();
 }
 
 void loop() {
-  //Storing Temperature and Humidity
-  DHT.read11(DHT11); //Reads Data from the DHT11 Sensor
-  Temp = DHT.temperature; //Stores Temperature in Temp
-  Humid = DHT.humidity; //Stores Humidity in Temp
+  //Storing Values
+  DHT.read11(DHT11); //Reading Data from the DHT11 Sensor
+  Temp = DHT.temperature; //Storing Temperature in Temp
+  Humid = DHT.humidity; //Storing Humidity in Temp
+  SensorMoist = analogRead(sensorPin); //Storing Moisture Level from the sensor - Range 0 to 1024
+  Moist = map(SensorMoist, 0, 1024, 0, 100); //The map function transforms the 0 to 1024 range to a 0 to 100 range
   
-  //Storing if motion is detected or not
+  //Checking if motion is detected or not
   PIR_State=digitalRead(PIR); //Reading the PIR sensor's output, whether high or low
-  if (PIR_State == 1){
-    digitalWrite(LED, HIGH); //If the Sensor is high it means motion is detected so we turn on the LED
+  if (PIR_State == 1){ //If the Sensor is high it means motion is detected
+    digitalWrite(LED, HIGH);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("WARNING! Motion is Detected!");
+    delay(3000);
+    lcd.clear();
   }
   else{
     digitalWrite(LED, LOW); //If the Sensor is low it means no motion is detected so we turn off the LED
   }
   
-  //Storing Moisture Lever
-  Moist = analogRead(sensorPin); //Moisture level stored in the variable moist
-
   //Printing Values in LCD Display
   lcd.setCursor(0,0);
   lcd.print("T:");
